4546859cb8a5c17b1389c1bf9168a36febcb19d3
pparksse1 <pparksse1@gmail.com>
2026-03-03 09:53:06 +0000

    chore: push project files

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-// Add JS here
+document.getElementById('generator-btn').addEventListener('click', () => {
+    const numbersDisplay = document.getElementById('numbers-display');
+    numbersDisplay.innerHTML = '';
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        numbers.add(Math.floor(Math.random() * 45) + 1);
+    }
+
+    for (const number of Array.from(numbers).sort((a, b) => a - b)) {
+        const circle = document.createElement('div');
+        circle.classList.add('number-circle');
+        circle.textContent = number;
+        numbersDisplay.appendChild(circle);
+    }
+});
